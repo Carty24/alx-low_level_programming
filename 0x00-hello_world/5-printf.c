b5f0235b5eeb2a0aea4705506f96adb0e155fb0c
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 10:50:49 -0800

    has to be better

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-/* I have to get this right */
+/**
+ *I have to get this right 
+ *main please ma ko ba me main
+*/
 int main(void)
 {
   /* main is nice */
-  printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
