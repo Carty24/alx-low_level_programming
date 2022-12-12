c3fe698db3afc4303b318ae0cf2eae379a5f508f
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 10:42:04 -0800

    good

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
+/* I have to get this right */
 int (void)
 {
-  /* using put to work no be beans */
-  char [please];
-
   printf("with proper grammar, but the outcome is a piece of art,\n");
   return (0);
     }
