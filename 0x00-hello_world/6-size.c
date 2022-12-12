e5b0cdcf4144e3235bbc7bfb8e3ea24090fd2b40
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 11:13:40 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main 
+ *
+ * Return: Always (0)
+ **/
+int main(voin)
+{
+  int i;
+    char c;
+    double d;
+
+    printf("The size of the integer is: %lu.\n", (long unsigned)sizeof (i));
+    printf("The size of the char is: %lu.\n", (long unsigned)sizeof (c));
+    printf("The size of the double is %lu.\n", (long unsigned)sizeof (d));
+
+    return(0);
+
+}
