0c83d46d6611064d648bef49d2fb8237ddd2fa21
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 10:35:26 -0800

    my c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int (void)
+{
+  /* using put to work no be beans */
+  char [please];
+
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return (0);
+    }
