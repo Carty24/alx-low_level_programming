8fd99ead31d62fedcd7ebd16a8341feed3a65fc6
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 11:41:01 -0800

    good work

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ char c;
 float f;
 long int li;
 long long int lli;
-printf("Size of the char: %lu.\n", (long unsigned)sizeof (c));
+printf("Size of a char: %lu.\n", (long unsigned)sizeof (c));
 printf("Size of an int: %lu.\n", (long unsigned)sizeof (i));
-printf("Size of the long int: %lu.\n", (long unsigned)sizeof (li));
-printf("Size of the long long int: %lu.\n", (long unsigned)sizeof (lli));
-printf("Size of the float: %lu.\n", (long unsigned)sizeof (f));
+printf("Size of a long int: %lu.\n", (long unsigned)sizeof (li));
+printf("Size of a long long int: %lu.\n", (long unsigned)sizeof (lli));
+printf("Size of a float: %lu.\n", (long unsigned)sizeof (f));
 return(0);
 }
