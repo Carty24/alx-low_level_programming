7fd4a8aff440afe6124e90f8ded989dad3fb985d
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 11:51:55 -0800

    kip going

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
 /**
- * main
+ * main Entry point
  *
- * Return: Always (0)
- **/
+ * Return: Always 0 (Success)
+ */
 int main(void)
+
 {
 int i;
 char c;
 float f;
 long int li;
 long long int lli;
-printf("Size of a char: %lu byte(s)\n", (long unsigned)sizeof (c));
-printf("Size of an int: %lu byte(s)\n", (long unsigned)sizeof (i));
-printf("Size of a long int: %lu byte(s)\n", (long unsigned)sizeof (li));
-printf("Size of a long long int: %lu byte(s)\n", (long unsigned)sizeof (lli));
-printf("Size of a float: %lu byte(s)\n", (long unsigned)sizeof (f));
+printf("Size of a char: %lu byte(s)\n",(long unsigned)sizeof (c));
+printf("Size of an int: %lu byte(s)\n",(long unsigned)sizeof (i));
+printf("Size of a long int: %lu byte(s)\n",(long unsigned)sizeof (li));
+printf("Size of a long long int: %lu byte(s)\n",(long unsigned)sizeof (lli));
+printf("Size of a float: %lu byte(s)\n",(long unsigned)sizeof (f));
 return(0);
 }
