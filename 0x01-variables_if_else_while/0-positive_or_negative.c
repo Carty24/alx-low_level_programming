9b3a1e65f089f525702fb00f504d1c5650da4bd5
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 15:39:54 -0800

    moving

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include <time.h>
+/**
+ * main - positive anything is better than negative nothing
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  int n;
+
+  srand(time(0));
+  n = rand() 0 RAND_MAX / 2;
+  /* your code goes there */
+  return (0);
+}
