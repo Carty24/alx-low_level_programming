1a97091d9599b1ef4585db29cfee81a90dca7ef2
Yusuf <damilareyusuf59@gmail.com>
2022-12-12 11:24:16 -0800

    improveing on c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 /**
- * main 
+ * main
  *
  * Return: Always (0)
  **/
 int main(voin)
 {
-  int i;
-    char c;
-    double d;
-
-    printf("The size of the integer is: %lu.\n", (long unsigned)sizeof (i));
-    printf("The size of the char is: %lu.\n", (long unsigned)sizeof (c));
-    printf("The size of the double is %lu.\n", (long unsigned)sizeof (d));
-
-    return(0);
-
+int i;
+char c;
+float f;
+longint li;
+longlongint lli;
+printf("The size of the integer is: %lu.\n", (long unsigned)sizeof (i));
+printf("The size of the char is: %lu.\n", (long unsigned)sizeof (c));
+printf("The size of the float is %lu.\n", (long unsigned)sizeof (f));
+printf("The size of the long int %lu.\n", (long unsinged)sizeof (li));
+prinf("The size of the long long int %lu.\n", (long unsigne)sizeof (lli));
+return(0);
 }
