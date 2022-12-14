96339827cc2fa17a18230c738276e211a102381b
Yusuf <damilareyusuf59@gmail.com>
2022-12-14 05:22:04 -0800

    good

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,15 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 char j, k;
 for (j=0; j<=9; j++)
 {
 for (k = 'a'; k <= 'z'; k++)
 {
-_punchar(k);
+_putchar(k);
 }
-_punchar('\n');
+_putchar('\n');
 }
 }
