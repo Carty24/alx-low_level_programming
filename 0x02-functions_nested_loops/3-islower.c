5058ead30798a11ecf88382efa697092dfda6c5b
Yusuf <damilareyusuf59@gmail.com>
2022-12-14 03:15:10 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,11 +2,13 @@
 /**
  * _islower - will check for lower case character
  *
+ * @c: is a lowercase character
+ *
  * Return: 0 otherwise
  */
 int _islower(int c)
 {
-if (c <= 'a' && c >= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
