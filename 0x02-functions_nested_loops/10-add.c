2c2477f1402a673c8fbd071a697c1dbf626b1fba
Yusuf <damilareyusuf59@gmail.com>
2022-12-14 17:25:36 -0800

    improve

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add -prining of multiple numbers
+ * @i: print i
+ * @j: print j
+ * Return: Always 0 (Success)
+ */
+int add(int i, int j)
+{
+return (i+j);
+}
