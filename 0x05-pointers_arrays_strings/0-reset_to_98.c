a4c5acca43f5b878987faed53aa5b6c7be379fe2
Yusuf <damilareyusuf59@gmail.com>
2022-12-19 08:57:05 -0800

    good work

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,3 @@
-#include <stdio.h>
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  int n;
-
-  n = 402;
-  printf("n=%d\n", n);
-  reset_to_98(&n);
-  printf("n=%d\n", n);
-  return (0);
-}
 #include "main.h"
 /**
  * reset_to_98-Resets the value of a variable back to 98.
