1cce268959066039e08c548ff303146fdbf38a65
Yusuf <damilareyusuf59@gmail.com>
2023-01-01 16:52:54 -0800

    modify print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
-#include "main.h"
+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#include "main.h"
 /**
  * main - print_alphabet_x10
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char j;
 for (j=0; j<=9; j++)
