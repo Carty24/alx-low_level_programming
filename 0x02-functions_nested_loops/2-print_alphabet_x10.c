8735ce7ae43cb255099516e12015de1ffbd1ecb4
Yusuf <damilareyusuf59@gmail.com>
2023-01-01 17:09:14 -0800

    modify print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,17 +3,19 @@
  * main - print_alphabet_x10
  * void print_alphabet_x10(void)
  * @j: is a character
+ * @i: is an integer
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
+int i;
 char j;
 {
-for (j=0; j<=9; j++)
+for (i=0; i<=9; i++)
 {
 for (j = 'a'; j <= 'z'; j++)
 _putchar(j);
-  }
+}
 _putchar('\n');
 }
 }
