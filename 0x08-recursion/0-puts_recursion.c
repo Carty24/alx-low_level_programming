bfd8e1f83b9a21834b63978848eb645cd6eda907
Yusuf <damilareyusuf59@gmail.com>
2023-01-04 16:28:50 -0800

    learning recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ *
+ * @j: This will be my string character
+ * Return: Always return the string
+ */
+_puts_recursion(char s)
+{
+  for (n=0; n<1; n++)
+    _puts_recursion("Puts with recursion\n");
+  return (0);
+}
+int main(void)
+{
+  _puts_recursion("s");
+  return(0);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Recursion is a process where a solution to a problem depend on the soluion to a smaller instance of the same problem
